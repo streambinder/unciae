c27f8a3bef221e0580ab21a6c7846b2abe892cf5
streambinder <posta@davidepucci.it>
2019-06-12 17:25:32 +0200

    backup: megabackup: first dump

---
+++ backup/megabackup/README.md
@@ -0,0 +1,77 @@
+# MEGABackup
+
+This is a very simple backup system based on `tar.xz` compression, with automatic files retention and that's using MEGA as backup destination.
+
+It backups common things such as databases (check `Databases dumper user setup` section), Apache virtual hosts, mail directories, filesystem ACLs and eventual arbitrary additional folders. Keep in mind, it has been written for a ISPConfig-configured webserver and it assumes several directory locations by defaults (such as `/var/www/vhost` symlink for virtual hosts, or `/var/vmail` for mail directories, and so on).
+
+The directory structure, inside the folder chosen as MEGA target directory, will be something like this:
+
+```
+|- 2018
+   |- 12
+      |- 31
+         |- acls.tar.xz
+         |- backup.log
+         |- databases.tar.xz
+         |- mailserver.tar.xz
+         |- webserver.tar.xz
+         |- folder1.tar.xz
+         |- folder2.tar.xz
+|- 2019
+   |- 01
+      |- 01
+         |- acls.tar.xz
+         |- backup.log
+         |- databases.tar.xz
+         |- mailserver.tar.xz
+         |- webserver.tar.xz
+         |- folder1.tar.xz
+         |- folder2.tar.xz
+      |- 02
+         |- acls.tar.xz
+         |- backup.log
+         |- databases.tar.xz
+         |- mailserver.tar.xz
+         |- webserver.tar.xz
+         |- folder1.tar.xz
+         |- folder2.tar.xz
+      |- 03
+         |- acls.tar.xz
+         |- backup.log
+         |- databases.tar.xz
+         |- mailserver.tar.xz
+         |- webserver.tar.xz
+         |- folder1.tar.xz
+         |- folder2.tar.xz
+```
+
+## How to use
+
+First of all, it relies on [`MEGATools`](https://megatools.megous.com) project: the script uses `megacopy` subcommand to synchronize backup on MEGA storage.
+Once installed, create a `~/.megarc` file (using the same user which will trigger the script run), with the following content:
+
+```ini
+[Login]
+Username = email@addre.ss
+Password = p4ssw0rd
+```
+
+In the first lines of the script you've the ability to tune few parameters, such as:
+1. `CONTACT_EMAIL`: if you want a backup report via email
+2. `BACKUP_VHOSTS`: to select which virtual hosts to backup
+3. `BACKUP_EXT_FOLDERS`: to backup any other additional path
+4. `BACKUP_RETENTION`: to set backup retention (months)
+5. `BANDWIDTH_SHAPING`: to set the maximum bandwidth available for the backup synchronization to MEGA
+6. `BACKUP_MEGADIR`: to set in which MEGA folder to save backups (it tipically starts with `/Root/`)
+
+### Databases dumper user setup
+
+In order to backup databases, assure you have a user with these privileges: `USAGE`, `SELECT`, `LOCK TABLES`, `SHOW VIEW`, `EVENT`, `TRIGGER`, `SHOW DATABASES`. Creating a dedicated one is straightforward:
+
+```sql
+GRANT USAGE, SELECT, LOCK TABLES,
+    SHOW VIEW, EVENT, TRIGGER, SHOW DATABASES
+    ON *.*
+    TO 'backup'@'localhost'
+    IDENTIFIED BY 'b4ckup';
+```
---
+++ backup/megabackup/runner.sh
@@ -0,0 +1,130 @@
+#!/bin/bash
+
+set -e
+export PATH=$PATH:/usr/local/bin
+
+CONTACT_EMAIL="email@addre.ss"
+BACKUP_VHOSTS="vhost1 vhost2"
+BACKUP_EXT_FOLDERS="/path/to/folder1 /path/to/folder2"
+BACKUP_RETENTION=2 # month(s)
+BANDWIDTH_SHAPING=1024 # kbps
+
+BACKUP_NAME="$(date +%Y-%m-%d)"
+BACKUP_LOWERBOUND="$(date --date="${BACKUP_RETENTION} months ago" +%s)"
+BACKUP_MEGADIR="/Root/server-backups/"
+DIR_OFFLINE="/tmp/backup_${BACKUP_NAME}"
+DIR_ONLINE="${BACKUP_MEGADIR}/${BACKUP_NAME//-//}"
+LOG_FILE="${DIR_OFFLINE}/backup.log"
+
+function mmkdir() {
+	awk -F'/' '{ for(i = 1; i <= NF; i++) { print $i; } }' <<< "$1" | grep -v ^$ | while read col; do
+		subpath="${subpath}/${col}"
+		megamkdir "$subpath" &>/dev/null || echo -n
+	done
+}
+function mmtopemptydir() {
+	dir_child="$1"
+	dir_parent="$(rev <<< "${dir_child}" | cut -d'/' -f 2- | rev)"
+	if [ "$(megals -R "${dir_parent}" | grep -v "^${dir_child}" | wc -l)" -gt 1 ]; then
+		echo "${dir_child}"
+	else
+		mmtopemptydir "${dir_parent}"
+	fi
+}
+
+mkdir -p "${DIR_OFFLINE}"
+exec &> >(tee -a "${LOG_FILE}")
+exec 2>&1
+
+# web sources
+if [ ! -f "${DIR_OFFLINE}/webserver.tar.xz" ]; then
+	echo "Backing up web sources"
+	cd /var/www
+	find -L ${BACKUP_VHOSTS} -type f -not -path '*/.git/*' \( -path '*/web/*' -o -path '*/private/*' \) -print0 | tar cvfJ "${DIR_OFFLINE}/webserver.tar.xz" --null -T -
+fi
+
+# mail
+if [ ! -f "${DIR_OFFLINE}/mailserver.tar.xz" ]; then
+	echo "Backing up mail data"
+	cd /var/vmail
+	tar cvfJ "${DIR_OFFLINE}/mailserver.tar.xz" $(grep -v mailfilters <<< "$(ls)" | xargs)
+fi
+
+# database
+if [ ! -f "${DIR_OFFLINE}/databases.tar.xz" ]; then
+	echo "Backing up databases"
+	cd "${DIR_OFFLINE}"
+	mysql -sN -u backup -p'b4ckup' -e "SHOW DATABASES;" \
+		| grep -Ev "(Database|information_schema|performance_schema)" \
+		| while read dbname; do
+		mysqldump -u backup -p'b4ckup' "$dbname" --add-drop-table > "${DIR_OFFLINE}/${dbname}.sql"
+	done
+	tar cvfJ "${DIR_OFFLINE}/databases.tar.xz" *.sql
+	rm -f *.sql
+fi
+
+# acls
+if [ ! -f "${DIR_OFFLINE}/acls.tar.xz" ]; then
+	echo "Backing up ACLs"
+	cd "${DIR_OFFLINE}"
+	set +e
+	getfacl -pR / > "${DIR_OFFLINE}/acls.txt" 2>/dev/null
+	set -e
+	tar cvfJ "acls.tar.xz" "acls.txt"
+	rm -f "acls.txt"
+fi
+
+# external folders
+if [ ! -z "${BACKUP_EXT_FOLDERS}" ]; then
+        for folder in ${BACKUP_EXT_FOLDERS}; do
+                echo "Backing up folder ${folder}"
+                folder_bkp="$(basename ${folder}).tar.xz"
+                if [ -f "${DIR_OFFLINE}/${folder_bkp}" ]; then
+                        continue
+                fi
+                cd "${folder}"
+                tar cvfJ "${DIR_OFFLINE}/${folder_bkp}" $(find -maxdepth 1 -not -name '.' -not -name '..' -printf "%f ")
+                cd -
+        done
+fi
+
+# recycling
+recycle_volumes=""
+empty_volumes=""
+while read -r backup_path; do
+	backup="$(sed "s|${BACKUP_MEGADIR}||g" <<< "${backup_path}" | cut -c 2- | sed 's|/|-|g')"
+	if [ "$(date --date="${backup}" +%s)" -lt "${BACKUP_LOWERBOUND}" ] \
+	&& [ "${#backup_path}" -gt "$(( ${#BACKUP_MEGADIR} + 1 ))" ]; then
+		echo "Removing ${backup_path}..."
+		recycle_volumes="${recycle_volumes} ${backup_path}"
+	else
+		echo "Recycling detection completed: ignoring volumes from ${backup_path} and newer."
+		break
+	fi
+done <<< "$(megals -R "${BACKUP_MEGADIR}" | egrep -e "^${BACKUP_MEGADIR}/[0-9]{4}/[0-9]{2}/[0-9]{2}$" | sort -u)"
+while read -r volume_path; do
+	if [ "$(megals -R "${volume_path}" | wc -l)" -le "1" ]; then
+		top_empty_volume_path="$(mmtopemptydir "${volume_path}")"
+		if [ "$(grep -c " ${top_empty_volume_path} " <<< "${empty_volumes}")" -eq 0 ]; then
+			echo "Removing (empty) ${top_empty_volume_path}..."
+			empty_volumes="${empty_volumes} ${top_empty_volume_path}"
+		fi
+	fi
+done <<< "$(megals -R "${BACKUP_MEGADIR}" | egrep /[0-9]+$)"
+if [ "$(( ${#recycle_volumes} + ${#empty_volumes} ))" -gt "$(( ${#BACKUP_MEGADIR} + 1 ))" ]; then
+	echo "Starting effecting volumes recycling..."
+	megarm ${recycle_volumes} ${empty_volumes}
+fi
+
+# synchronizing
+echo "Creating online directory ${DIR_ONLINE}"
+mmkdir "${DIR_ONLINE}"
+echo "Pushing data to MEGA"
+megacopy --limit-speed="${BANDWIDTH_SHAPING}" --local "${DIR_OFFLINE}" --remote "${DIR_ONLINE}"
+
+if [ ! -z "${CONTACT_EMAIL}" ]; then
+	cat "${LOG_FILE}" | mail -s "MEGA backup outcome" "${CONTACT_EMAIL}"
+fi
+
+echo "Removing local backup"
+rm -rf "${DIR_OFFLINE}"

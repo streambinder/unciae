f92c5c1d9594d16bdbeb8e1725bb88df03497d2b
streambinder <posta@davidepucci.it>
2019-07-04 15:14:36 +0200

    git: history-touchup: first dump

---
+++ git/history-touchup/README.md
@@ -0,0 +1,9 @@
+# History Touch-up
+
+This tool helps modifying the Git repository history without messing up with commits original timestamps.
+
+## How to use
+
+```bash
+history-touchup/runner.sh
+```
---
+++ git/history-touchup/runner.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# auxiliary functions
+
+function help() { echo -e "Usage:\n\t$(basename $0)"; exit 0; }
+function rprint() { echo -en "\r\e[0K$@"; }
+function pprint() { echo -e "\r\e[0K$@"; }
+
+# shell setup
+
+# arguments parsing
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -h|--help)
+            help
+            ;;
+    esac
+    shift
+done
+
+# arguments validation
+
+# effective script
+
+git rebase -i --root
+git filter-branch --env-filter '
+    GIT_COMMITTER_DATE=$GIT_AUTHOR_DATE;
+    export GIT_COMMITTER_DATE'

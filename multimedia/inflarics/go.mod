260913aee2ccd32929900ec7930595f9f992e0af
streambinder <posta@davidepucci.it>
2023-07-14 16:50:25 +0200

    feat(inflarics): rewrite against latest upstream

--- multimedia/inflarics/go.mod
+++ multimedia/inflarics/go.mod
@@ -3,24 +3,25 @@ module github.com/streambinder/unciae/multimedia/inflarics
 go 1.19
 
 require (
-	github.com/bogem/id3v2 v1.2.0
-	github.com/sirupsen/logrus v1.9.0
-	github.com/streambinder/spotitube v0.0.0-20220930132717-38edc576b0ee
+	github.com/bogem/id3v2/v2 v2.1.4
+	github.com/gosimple/slug v1.13.1
+	github.com/spf13/cobra v1.7.0
+	github.com/streambinder/spotitube v1.0.2
 )
 
 require (
-	github.com/0xAX/notificator v0.0.0-20220220101646-ee9b8921e557 // indirect
-	github.com/PuerkitoBio/goquery v1.8.0 // indirect
-	github.com/andybalholm/cascadia v1.3.1 // indirect
-	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/gosimple/slug v1.12.0 // indirect
+	github.com/PuerkitoBio/goquery v1.8.1 // indirect
+	github.com/adrg/xdg v0.4.0 // indirect
+	github.com/agnivade/levenshtein v1.1.1 // indirect
+	github.com/andybalholm/cascadia v1.3.2 // indirect
+	github.com/arunsworld/nursery v0.6.0 // indirect
 	github.com/gosimple/unidecode v1.0.1 // indirect
-	github.com/rainycape/unidecode v0.0.0-20150907023854-cb7f23ec59be // indirect
-	github.com/zmb3/spotify v1.3.0 // indirect
-	golang.org/x/net v0.7.0 // indirect
-	golang.org/x/oauth2 v0.0.0-20220909003341-f21342109be1 // indirect
-	golang.org/x/sys v0.5.0 // indirect
-	golang.org/x/text v0.7.0 // indirect
-	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/protobuf v1.28.1 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/net v0.12.0 // indirect
+	golang.org/x/sys v0.10.0 // indirect
+	golang.org/x/text v0.11.0 // indirect
 )
--- multimedia/inflarics/runner.go
+++ multimedia/inflarics/runner.go
@@ -1,123 +1,69 @@
 package main
 
 import (
-	"flag"
+	"errors"
 	"fmt"
-	"strings"
+	"log"
 
-	"github.com/bogem/id3v2"
-	"github.com/sirupsen/logrus"
+	"github.com/bogem/id3v2/v2"
+	"github.com/gosimple/slug"
+	"github.com/spf13/cobra"
+	"github.com/streambinder/spotitube/entity"
+	"github.com/streambinder/spotitube/entity/id3"
 	"github.com/streambinder/spotitube/lyrics"
-	"github.com/streambinder/spotitube/track"
+	"github.com/streambinder/spotitube/util"
 )
-
-var (
-	argFilename string
-	argArtist   string
-	argTitle    string
-	argVerbose  bool
-
-	log *logrus.Logger
-)
-
-func init() {
-	// logger setup
-	log = logrus.New()
-
-	// args parse
-	flag.StringVar(&argArtist, "artist", "", "Artist name")
-	flag.StringVar(&argTitle, "title", "", "Track title")
-	flag.BoolVar(&argVerbose, "verbose", false, "Verbose mode")
-	flag.Parse()
-
-	if argVerbose {
-		log.SetLevel(logrus.DebugLevel)
-	}
-
-	if len(flag.Args()) == 0 {
-		log.Fatalln("At least a filename must be given")
-	}
-
-	argFilename = flag.Args()[0]
-}
 
 func main() {
-	if argArtist == "" || argTitle == "" {
-		artist, title, err := id3Parse(argFilename)
-		if err != nil {
-			log.WithError(err).Fatalln()
-		}
-		argArtist = artist
-		argTitle = title
-	}
+	cmd := &cobra.Command{
+		Use:   "inflarics",
+		Short: "Fetch and insert lyrics of tracks into MP3 files metadata",
+		Args:  cobra.MinimumNArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			for _, path := range args {
+				tag, err := id3.Open(path, id3v2.Options{Parse: true})
+				if err != nil {
+					return err
+				}
+				defer tag.Close()
 
-	log.WithFields(logrus.Fields{
-		"artist": argArtist,
-		"title":  argTitle,
-	}).Println("Searching lyrics for", argFilename)
-	lyrics, err := fetchLyrics(argArtist, argTitle)
-	if err != nil {
-		log.WithError(err).Fatalln()
-	} else if len(lyrics) < 10 {
-		log.Fatalln("Lyrics is too short")
-	} else {
-		log.WithField("lyrics", fmt.Sprintf("%s...", string(lyrics[:10]))).Debugln("Lyrics found")
-	}
+				artist, _ := cmd.Flags().GetString("artist")
+				artist = util.Fallback(artist, tag.Artist())
+				if len(artist) == 0 {
+					return errors.New("artist name is mandatory")
+				}
 
-	if err := id3Persist(argFilename, lyrics); err != nil {
-		log.WithError(err).Fatalln()
-	}
+				title, _ := cmd.Flags().GetString("title")
+				title = util.Fallback(title, tag.Title())
+				if len(artist) == 0 {
+					return errors.New("track title is mandatory")
+				}
 
-	log.Println("Track inflated.")
+				log.Printf("Searching lyrics for %s by %s...\n", title, artist)
+				lyrics, err := lyrics.Search(&entity.Track{ID: util.Fallback(tag.SpotifyID(), fakeID(artist, title)), Title: title, Artists: []string{artist}})
+				if err != nil {
+					return err
+				}
+
+				if len(lyrics) == 0 {
+					log.Printf("Lyrics not found\n")
+					continue
+				}
+				log.Printf("Found: %s\n", util.Excerpt(lyrics, 50))
+
+				tag.SetUnsynchronizedLyrics(title, lyrics)
+				if err := tag.Save(); err != nil {
+					return err
+				}
+			}
+			return nil
+		},
+	}
+	cmd.Flags().StringP("artist", "a", "", "Artist name")
+	cmd.Flags().StringP("title", "t", "", "Track title")
+	cmd.Execute()
 }
 
-func id3Parse(filename string) (string, string, error) {
-	track, err := track.OpenLocalTrack(filename)
-	if err != nil {
-		return "", "", err
-	}
-
-	return track.Artist, track.Title, nil
-}
-
-func id3Persist(filename, lyrics string) error {
-	log.Debugln("Opening file for ID3 flushing")
-	tag, err := id3v2.Open(filename, id3v2.Options{Parse: true})
-	if err != nil {
-		return err
-	}
-
-	tag.AddUnsynchronisedLyricsFrame(id3v2.UnsynchronisedLyricsFrame{
-		Encoding:          id3v2.EncodingUTF8,
-		Language:          "eng",
-		ContentDescriptor: tag.Artist(),
-		Lyrics:            lyrics,
-	})
-
-	if err := tag.Save(); err != nil {
-		return err
-	}
-	if err := tag.Close(); err != nil {
-		return err
-	}
-	return nil
-}
-
-func fetchLyrics(artist, title string) (string, error) {
-	for _, p := range lyrics.All() {
-		log.WithField("provider", p.Name()).Debugln("Querying provider")
-
-		text, err := p.Query(argTitle, argArtist)
-		if err != nil {
-			log.WithError(err).Errorln()
-			continue
-		} else if len(strings.TrimSpace(text)) == 0 {
-			log.WithField("provider", p.Name()).Errorln("Lyrics too short, skipping")
-			continue
-		}
-
-		return text, nil
-	}
-
-	return "", fmt.Errorf("Lyrics not found")
+func fakeID(artist, title string) string {
+	return slug.Make(fmt.Sprintf("inflarics-%s-%s", artist, title))
 }

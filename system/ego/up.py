d4c76b4afe42b64cf052a82985afa0097eb386c9
streambinder <posta@davidepucci.it>
2024-10-15 13:10:54 +0200

    feat(ego): share sudo shell across all deps in need

--- system/ego/up.py
+++ system/ego/up.py
@@ -6,17 +6,52 @@ import hashlib
 import os
 import platform
 import shutil
+from getpass import getpass
 from typing import Any, AsyncGenerator, List, Optional, Tuple
 
 import asyncclick as click
-from termcolor import COLORS as colors
 from termcolor import colored
 
-COLORS = [color for color in colors.keys() if color not in ["black", "grey", "red"]]
+SHELL_COLORS = [
+    "green",
+    "yellow",
+    "blue",
+    "magenta",
+    "cyan",
+    "light_green",
+    "light_yellow",
+    "light_blue",
+    "light_magenta",
+    "light_cyan",
+]
 
 
 def color_by_name(keyword: str) -> str:
-    return COLORS[sum(hashlib.md5(keyword.encode()).hexdigest().encode()) % len(COLORS)]
+    return SHELL_COLORS[
+        sum(hashlib.md5(keyword.encode()).hexdigest().encode()) % len(SHELL_COLORS)
+    ]
+
+
+def only_once(func):
+    _run_attr = "_run"
+    _value_attr = "_value"
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        if getattr(func, _run_attr, False):
+            return getattr(func, _value_attr, None)
+
+        value = func(*args, **kwargs)
+        setattr(func, _value_attr, value)
+        setattr(func, _run_attr, True)
+        return value
+
+    return wrapper
+
+
+@only_once
+def get_pass(program: Optional[str] = None) -> str:
+    return getpass(f"Authenticate{' for ' + program if program else ''}: ")
 
 
 def is_os(os_name: str) -> bool:
@@ -41,43 +76,53 @@ async def print_stream(stream, program, color: Optional[str] = None) -> None:
         print_line(program, line.decode().strip(), color)
 
 
+async def write_stdin(stdin: Optional[asyncio.StreamWriter] = None) -> None:
+    if not stdin:
+        return
+
+    stdin.writelines([get_pass().encode("utf-8") + b"\n"] * 100)
+    await stdin.drain()
+
+
 def dep(
-    cmd: str | None = None, platform_name: str = "", envs: Optional[List[str]] = None
+    cmd: str | None = None,
+    platform_name: str = "",
+    env: Optional[List[str]] = None,
 ):
-    has_os = is_os(platform_name)
-    has_envs = len([env for env in (envs or []) if env not in os.environ]) == 0
+    valid_os = is_os(platform_name)
+    valid_env = len([e for e in (env or []) if e not in os.environ]) == 0
 
     def decorator_dep(func):
         program = func.__name__
-        has_cmd = is_exec(cmd or program)
+        valid_cmd = is_exec(cmd or program)
 
         @functools.wraps(func)
         async def wrapper_dep(*args: Any, **kwargs: Any):
-            if not (has_cmd and has_os and has_envs):
+            if not (valid_cmd and valid_os and valid_env):
                 print_line(program, "is unsupported")
                 return
 
             print_line(program, "upgrading...")
-
             async for p_args, p_kwargs in func(*args, **kwargs):
-                if p_args[0].startswith("sudo"):
-                    await asyncio.create_subprocess_exec(
-                        "sudo", "-p", "Password: ", "echo", "-n"
-                    )
+                sudo = p_args[0] == "sudo"
+                if sudo:
+                    get_pass(program)
+                    p_args.insert(1, "-S")
 
                 process = await asyncio.create_subprocess_exec(
                     *p_args,
+                    stdin=asyncio.subprocess.PIPE if sudo else None,
                     stdout=asyncio.subprocess.PIPE,
                     stderr=asyncio.subprocess.PIPE,
                     **p_kwargs,
                 )
-
                 await asyncio.gather(
+                    write_stdin(process.stdin if sudo else None),
                     print_stream(process.stdout, program),
                     print_stream(process.stderr, program, "red"),
                 )
 
-            print_line(program, "done.")
+            print_line(program, "upgrade complete.")
 
         return wrapper_dep
 
@@ -109,14 +154,14 @@ async def managedsoftwareupdate() -> AsyncGenerator[Tuple[list, dict], None]:
     yield ["sudo", "managedsoftwareupdate", "--installonly"], {}
 
 
-@dep(cmd="zsh", envs=["ZSH"])
+@dep(cmd="zsh", env=["ZSH"])
 async def omz() -> AsyncGenerator[Tuple[list, dict], None]:
     yield [f"{os.getenv('ZSH')}/tools/upgrade.sh"], {}
 
 
 @dep(platform_name="darwin")
 async def softwareupdate() -> AsyncGenerator[Tuple[list, dict], None]:
-    yield ["softwareupdate", "-i", "-a"], {}
+    yield ["sudo", "softwareupdate", "-iaR"], {}
 
 
 @dep()

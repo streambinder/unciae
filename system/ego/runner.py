a5d7b5ef7eb410675d58b23c9744e95c1d640ee9
streambinder <posta@davidepucci.it>
2025-10-20 19:41:52 +0200

    feat(ego): drop id subcommand

--- system/ego/runner.py
+++ system/ego/runner.py
@@ -2,7 +2,6 @@
 
 import anyio
 import asyncclick as click
-from id import cmd_id
 from up import cmd_up
 
 
@@ -10,7 +9,6 @@ from up import cmd_up
 async def ego(): ...
 
 
-ego.add_command(cmd_id)
 ego.add_command(cmd_up)
 
 if __name__ == "__main__":

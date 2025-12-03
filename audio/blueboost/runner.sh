4f0d19aee6218e5cc9de849d6e532343cc95f2ba
streambinder <posta@davidepucci.it>
2025-12-03 11:40:10 +0100

    chore(blueboost): drop

--- audio/blueboost/runner.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-dbus-send --system --print-reply --dest=org.bluez / org.freedesktop.DBus.ObjectManager.GetManagedObjects |
-	awk -F'"' '/\/(sep|fd)/ {print $2}' |
-	while read -r addr; do
-		dbus-send --print-reply --system --dest=org.bluez "${addr}" \
-			org.freedesktop.DBus.Properties.Set string:org.bluez.MediaTransport1 string:Volume variant:uint16:127
-	done

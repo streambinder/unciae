6bace350fa0cf9cf2add0b0ee96ae3d0512e428a
streambinder <automata@davidepucci.it>
2026-09-18 10:41:21 +0200

    fix(apto): stop stripping jpeg trailers
    Stripping the trailer deletes a motion photo's embedded video but leaves stale markers behind.

--- media/apto/main.sh
+++ media/apto/main.sh
@@ -344,7 +344,6 @@ while read -r fname <&3; do
 	final_fname="${final_fname/ /-}"
 
 	# perform the changes
-	[[ "${final_ext}" != "jpg" ]] || exiftool -overwrite_original -m -trailer= "${fname}"
 	exiftool -api QuickTimeUTC=1 -overwrite_original -m -wm w \
 		-time:all="${final_exif_timestamp}" "${fname}" &&
 		exiftool -api QuickTimeUTC=1 -overwrite_original -m \
